cb64119339a33690fe3f7a2a79aaac734a0e961d
lynx42 <bjarnhildar@gmail.com>
2020-07-27 22:42:08 +0200

    Array_IteratingOverArray Completing theory by adding for-loop, for-each-loop and for-loop that does what for-each-loop does.

--- src/Array_IteratingOverArray.java
+++ src/Array_IteratingOverArray.java
@@ -3,26 +3,68 @@ import java.util.Arrays;
 
 public class Array_IteratingOverArray {
     public static void main(String[] args) {
-        array1();
+        forEachLoop();
+        forLoop();
+        scannerToArray();
+        isBroken();
     }
 
-    // Example 1. Filling an array with the squares of indexes of its elements.
-    public static void array1() {
-        int n = 10; // size of an array
-        int[] squares = new int[n]; // creating an array with the specific size of n
+    public static void forEachLoop() {
+        char[] characters = {'a', 'b', 'c', 'a', 'b', 'c', 'a'}; // creating an array named "characters", containing some characters
 
-        System.out.println(Arrays.toString(squares)); // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-        /* iteratinf over the array */
-        for (int i = 0; i < squares.length; i++) {
-            squares[i] /* defines the arrays index position to modify*/ = i * i /* defines what to put into that position*/;
+        int counter = 0; // to count the amount of "a" in the array
+        for (char ch : characters) { // reads: for each element ch of type char in the array characters do some statements in the body
+            if (ch == 'a') { // when will some event be triggered?
+                counter++; // what should happen, when triggered?
+            }
         }
-        System.out.println(Arrays.toString(squares));
+     System.out.println(counter); // it outputs "3"
     }
 
-    // Example 2. Checking the order of elements.
-    public static void array2() {
+    public static void forLoop() {
+        char [] characters = {'a', 'b', 'c', 'a', 'b', 'c', 'a'};
 
+        int counter = 0;
+        for (int i = 0; i < characters.length; i++) {
+            if (characters[i] == 'a') {
+                counter++;
+            }
+        }
+        System.out.println(counter);
     }
+    public static void scannerToArray() {
+        Scanner scanner = new Scanner(System.in);
+
+        int defineLength = scanner.nextInt(); // reading a length
+        int[] array = new int[defineLength]; // creating an array with the specific length
+
+        for (int i = 0; i < defineLength; i++) {
+            array[i] = scanner.nextInt(); // read the next number to be put into the array
+        }
+        System.out.println(Arrays.toString(array)); // output the array
+    }
+
+    public static void isBroken() {
+        int[] numbers = {1, 2, 3, 4, 5, 10, 6}; // the order is broken
+
+        boolean broken = false; // suppose the array is well-ordered
+
+        /* iterating over the array */
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] < numbers[i - 1]) { // if the order is broken
+                broken = true; // write a result
+                break; // terminate the loop
+            }
+        }
+
+        if (broken) {
+            System.out.println("BROKEN");
+        } else {
+            System.out.println("OK");
+        }
+    }
+
+
 }
 
