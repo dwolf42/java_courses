817d8e71eea3af08528f52748786e9cd74f07c7c
lynx42 <bjarnhildar@gmail.com>
2020-08-02 22:18:37 +0200

    ArrayForLoop_LongestAscendingSequence: tying another approach to crack this baby

--- src/ArrayForLoop_LongestAscendingSequence.java
+++ src/ArrayForLoop_LongestAscendingSequence.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class ArrayForLoop_LongestAscendingSequence {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int defineLength = scanner.nextInt();
         int longestSequence = 0;
         int indexOnSave = 0;
-        int indexOnSaveToPrint = 0;
+        int sequenceToPrint = 0;
+        //int indexOnSaveToPrint = 0;
 
         int[] arrayForInput = new int[defineLength];
         int[] saveLongestSequence = new int[defineLength];
@@ -16,29 +18,49 @@ public class ArrayForLoop_LongestAscendingSequence {
             arrayForInput[i] = scanner.nextInt();
         }
 
-        for (int j = 1; j < arrayForInput.length; j++) {
+        for (int j = 0; j < arrayForInput.length; j++) {
 
-            if (arrayForInput[j] > arrayForInput[j - 1]) {
-                longestSequence++;
-            } else {
-                saveLongestSequence[indexOnSave] = longestSequence;
-                indexOnSave++;
-                longestSequence = 1;
+            for (int k = 1; k < arrayForInput.length; k++) {
+
+                if (arrayForInput[k] > arrayForInput[k - 1]) {
+                    saveLongestSequence[j]++;
+                } else {
+                    break;
+/*                } else {
+                 saveLongestSequence[indexOnSave] = longestSequence;
+                 indexOnSave++;
+                 longestSequence = 1;
+                }
+*/
+                }
             }
         }
 
-        for (int k = 1; k < saveLongestSequence.length; k++) {
+        for (int l = 0; l < saveLongestSequence.length; l++) {
 
-            if (saveLongestSequence[k] > saveLongestSequence[k - 1]) {
-                indexOnSaveToPrint = k;
+            if (saveLongestSequence[l] > sequenceToPrint) {
+                sequenceToPrint = saveLongestSequence[l];
+            }
+        }
+
+/*
+            if (saveLongestSequence[l] > saveLongestSequence[l - 1]) {
+                indexOnSaveToPrint = l;
             }
         }
         if (saveLongestSequence[indexOnSaveToPrint] > saveLongestSequence[0]) {
             System.out.println(saveLongestSequence[indexOnSaveToPrint]);
         } else {
             System.out.println(saveLongestSequence[0]);
-        }
+*/
+/*        }
 
+        if (sequenceToPrint > longestSequence) {
+            System.out.println(sequenceToPrint);
+        } else {
+            System.out.println(longestSequence);
+        }
+*/
+        System.out.println(sequenceToPrint);
     }
 }
-
