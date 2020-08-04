acbd19c130957b008b9d2553fd968d2516ac3a0b
lynx42 <bjarnhildar@gmail.com>
2020-08-05 00:01:30 +0200

    ArrayForLoop_LongestAscendingSequence: Finally did it! Lesson completed

--- src/ArrayForLoop_LongestAscendingSequence.java
+++ src/ArrayForLoop_LongestAscendingSequence.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class ArrayForLoop_LongestAscendingSequence {
 
@@ -6,33 +7,24 @@ public class ArrayForLoop_LongestAscendingSequence {
         Scanner scanner = new Scanner(System.in);
 
         int defineLength = scanner.nextInt();
-        int longestSequence = 0;
         int indexOnSave = 0;
         int sequenceToPrint = 0;
-        //int indexOnSaveToPrint = 0;
 
         int[] arrayForInput = new int[defineLength];
         int[] saveLongestSequence = new int[defineLength];
+
+        Arrays.fill(saveLongestSequence, 1);
 
         for (int i = 0; i < arrayForInput.length; i++) {
             arrayForInput[i] = scanner.nextInt();
         }
 
-        for (int j = 0; j < arrayForInput.length; j++) {
+        for (int k = 1; k < arrayForInput.length; k++) {
 
-            for (int k = 1; k < arrayForInput.length; k++) {
-
-                if (arrayForInput[k] > arrayForInput[k - 1]) {
-                    saveLongestSequence[j]++;
-                } else {
-                    break;
-/*                } else {
-                 saveLongestSequence[indexOnSave] = longestSequence;
-                 indexOnSave++;
-                 longestSequence = 1;
-                }
-*/
-                }
+            if (arrayForInput[k] > arrayForInput[k - 1]) {
+                saveLongestSequence[indexOnSave]++;
+            } else {
+                indexOnSave++;
             }
         }
 
@@ -43,24 +35,30 @@ public class ArrayForLoop_LongestAscendingSequence {
             }
         }
 
-/*
-            if (saveLongestSequence[l] > saveLongestSequence[l - 1]) {
-                indexOnSaveToPrint = l;
-            }
-        }
-        if (saveLongestSequence[indexOnSaveToPrint] > saveLongestSequence[0]) {
-            System.out.println(saveLongestSequence[indexOnSaveToPrint]);
-        } else {
-            System.out.println(saveLongestSequence[0]);
-*/
-/*        }
-
-        if (sequenceToPrint > longestSequence) {
-            System.out.println(sequenceToPrint);
-        } else {
-            System.out.println(longestSequence);
-        }
-*/
         System.out.println(sequenceToPrint);
     }
 }
+/*
+Write a program that reads an array of ints and outputs the length of the longest sequence in strictly ascending order. Elements of the sequence must go one after
+another. A single number is assumed to be ordered sequence with the length = 1.
+
+Input data format
+
+The first line contains the size of an array.
+The second line contains elements of the array separated by spaces.
+
+Example
+
+The input array is 1 2 4 1 2 3 5 7 4 3. In this case, the length of the longest sequence in ascending order is 5. It includes the following elements: 1 2 3 5 7.
+
+Sample Input 1:
+
+10
+
+1 2 4 1 2 3 5 7 4 3
+
+Sample Output 1:
+
+5
+
+*/
