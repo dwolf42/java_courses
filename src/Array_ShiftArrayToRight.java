a5aa5689c52afcfd61f165f33beba2a5b210d71b
Kabraxis Luvos <kabraxis.luvos@gmail.com>
2020-10-27 20:57:09 +0100

    Program that rotates an array to the right the desired amount of times

---
+++ src/Array_ShiftArrayToRight.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+
+public class Array_ShiftArrayToRight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] oldArray = {5, 3, 7, 9};
+        int[] newArray = shiftMultiple(oldArray, scanner.nextInt());
+        printArray(newArray);
+    }
+
+    //Pushes the array the desired amount of times through the sifter.
+    public static int[] shiftMultiple(int[] arr, int count) {
+        for (int i = 0; i < count; i++) {
+            shiftArrayRight(arr);
+        }
+        return arr;
+    }
+
+    //Takes in an array of integers then returns a new array
+    //with all the values shifted right.
+    public static int[] shiftArrayRight(int[] array1) {
+        //Saves the last position of the array in a temporary variable
+        int tempVariable = array1[array1.length - 1];
+        //Loop starts at the 2nd-last pos. of array1 (number 7).
+        for (int i = array1.length - 2; i > -1; i--) {
+            //At each iteration of the loop copy the element of position i to position i + 1.
+            array1[i + 1] = array1[i];
+        }
+        //Finally copy value of tempVariable to first position of the array1 and return the array.1
+        array1[0] = tempVariable;
+        return array1;
+    }
+
+    //Takes an array of integers and prints it to console
+    public static void printArray(int[] arrr) {
+        for (int x : arrr)
+            System.out.print(x + ",");
+    }
+}
+
+
+
+
