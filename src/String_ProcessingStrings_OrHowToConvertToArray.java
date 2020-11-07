679abcacc4cf3428d241c5d4b3780893b2bfedc4
Kabraxis Luvos <46925680+Kabraxis@users.noreply.github.com>
2020-11-07 13:23:56 +0100

    Program that rotates an array to the right the desired amount of times.
    Also some practicing of String processing or How to convert a String to an Array

--- src/String_ProcessingStrings_OrHowToConvertToArray.java
+++ src/String_ProcessingStrings_OrHowToConvertToArray.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class String_ProcessingStrings_OrHowToConvertToArray {
     public static void main(String[] args) {
         char[] chars = {'A', 'B', 'C', 'D', 'E', 'F'};
-        System.out.println("chars: " + Arrays.toString(chars));
 
         String stringFromChars = String.valueOf(chars);
         System.out.println("stringFromChars: " + stringFromChars);
