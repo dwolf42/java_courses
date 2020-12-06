aa844154166cc2bf35eff8f9ad93407072564e59
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2020-12-06 22:34:33 +0100

    Version 3 puts the user input directly into an array whilst it is split at the white space

--- src/Array/Array_ShiftArrayToRight3.java
+++ src/Array/Array_ShiftArrayToRight3.java
@@ -42,9 +42,11 @@ Iterating over arrays  Right Rotation
 
 A right rotation is an operation that shifts each element of an array to the right. For example, if an array is {1,2,3,4,5} and we right rotate it by 1, the new array will be {5,1,2,3,4}. If we rotate it by 2, the new array will be {4,5,1,2,3}. It goes like this: {1,2,3,4,5} -> {5,1,2,3,4} -> {4,5,1,2,3}.
 Write a program that performs a right rotation on an array by a given number.
+
 Input format:
 The first line is an array of numbers.
 The second line is the number of rotations.
+
 Output format:
 Resulting array
 
