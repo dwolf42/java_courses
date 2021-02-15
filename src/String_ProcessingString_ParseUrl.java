93a8bd005946a0d796538d32f53656cd94041d62
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-02-15 08:40:21 +0100

    Update

--- src/String_ProcessingString_ParseUrl.java
+++ src/String_ProcessingString_ParseUrl.java
@@ -13,11 +13,6 @@ public class String_ProcessingString_ParseUrl {
         int indexOfPass = -1;
 
         for (int i = 0; i < split2.length; i++) {
-            // This part of code comes into play if the URL contains the parameter "pass",
-            // it is necessary to save the index-position of pass in due to the requirement of processing "pass" different
-            // than other parameters. Also in order to avoid "pass" being processed like other parameters the loop needs
-            // to skip the current iteration.
-            // Further details see next part.
             if (split2[i].contains("pass")) {
                 char[] passChars = split2[i].toCharArray();
                 for (int j = 0; j < passChars.length; j++) {
@@ -29,10 +24,6 @@ public class String_ProcessingString_ParseUrl {
                 }
                 continue;
             }
-            // Here we have the code that processes every parameter that is not "pass" and its values
-            // What the part for "pass" and this one here basically do is to iterate over ever index of array split2
-            // and since they all contain a string-type value these strings can be split into character arrays.
-            // Now the code iterates over the array, checks if there is a '=' and replaces it with the required sentence.
             char[] notPassChars = split2[i].toCharArray();
             for (int k = 0; k < notPassChars.length; k++) {
                 if (k == notPassChars.length - 1 && notPassChars[k] == '=') {
@@ -44,7 +35,6 @@ public class String_ProcessingString_ParseUrl {
                 }
             }
         }
-        // This is also part of the treatment for our special snowflake "pass".
         if (indexOfPass > -1) {
             System.out.println(split2[indexOfPass].replace("pass=", "password : "));
         }
@@ -80,3 +70,4 @@ cookie : not found
 host : localhost
 
 */
+
