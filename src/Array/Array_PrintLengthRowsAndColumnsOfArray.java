4384cfc30017bc9e9dd232e70575b61eb96923d3
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-02-16 22:57:37 +0100

    new part

---
+++ src/Array/Array_PrintLengthRowsAndColumnsOfArray.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class Array_PrintLengthRowsAndColumnsOfArray {
+    public static void main(String[] args) {
+        int[][] c = {
+                {40, 30, 25, 30, 15},
+                {13, 12, 16},
+                {101, 125, 114, 131}
+        };
+        System.out.print(c.length + " "); // prints how many rows the 2d array has
+
+        for (int[] nestedArray : c) {
+            System.out.print(nestedArray.length + " "); // prints how many columns the array has - since we print only the 1d array parts of the array.
+        }
+    }
+}
