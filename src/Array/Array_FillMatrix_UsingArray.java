246cc84dcf96f7da19354287afe5778ae90eefa8
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-02-23 17:23:57 +0100

    GOT IT! BOOM BABY!! :D

--- src/Array/Array_FillMatrix_UsingArray.java
+++ src/Array/Array_FillMatrix_UsingArray.java
@@ -1,19 +1,30 @@
 package Array;
+
 import java.util.Scanner;
 
 public class Array_FillMatrix_UsingArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int defineLength = 5;
+        int defineLength = 10;
         int[][] matrix = new int[defineLength][defineLength];
 
-         for (int i = 0; i < matrix.length; i++) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            int m = 0;
             int j = i;
             for (; j >= 0; j--) {
-                System.out.print(j + " ");
+                matrix[i][m] = j;
+                m++;
             }
             for (int k = j + 2; k < matrix.length - i; k++) {
-                System.out.print(k + " ");
+                matrix[i][m] = k;
+                m++;
+            }
+            System.out.println();
+        }
+        for (int p = 0; p < matrix.length; p++) {
+            for (int q = 0; q < matrix.length; q++) {
+                System.out.print(matrix[p][q] + " ");
             }
             System.out.println();
         }
