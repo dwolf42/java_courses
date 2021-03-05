ca29b3d128231cd5933d99897516ecb76edbc203
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-03-05 13:17:02 +0100

    implementing Scanner class for user input

--- src/Array/ArrayTwoDimensional_SwapColumns.java
+++ src/Array/ArrayTwoDimensional_SwapColumns.java
@@ -1,19 +1,46 @@
 package Array;
 
+import java.util.Scanner;
+
 public class ArrayTwoDimensional_SwapColumns {
-    public static void main(String[] args){
-        int row = 3;
-        int column = 4;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int row = scanner.nextInt();
+        int column = scanner.nextInt();
         int swap1 = 0;
         int swap2 = 1;
 
-        int[][] matrix = {
-            {11, 12, 13, 14},
-            {21, 22, 23, 24},
-            {31, 32, 33, 34}
-        };
+        int[][] matrix = new int[row][column];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; i < matrix[i].length; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
 
         int[][] swapMatrix = new int[row][column];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (j == swap1) {
+                    swapMatrix[i][j] = matrix[i][swap2];
+                    continue;
+                }
+                if (j == swap2) {
+                    swapMatrix[i][j] = matrix[i][swap1];
+                    continue;
+                }
+                swapMatrix[i][j] = matrix[i][j];
+            }
+        }
+        for (int[] nums : swapMatrix) {
+            for (int nbrs : nums) {
+                System.out.print(nbrs + " ");
+            }
+            System.out.println();
+        }
     }
 }
 /*
