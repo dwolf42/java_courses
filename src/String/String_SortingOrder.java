6511aab132d98361ddd24f2e4ee7e960996a0c7e
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-05-16 23:45:21 +0200

    implemented scanner, next: check if input is null

--- src/String/String_SortingOrder.java
+++ src/String/String_SortingOrder.java
@@ -1,39 +1,42 @@
 package String;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class String_SortingOrder {
     public static void main(String[] args) {
-        String dummyInput = "is2 Thi1s T4est 3a";
-        String[] input = dummyInput.split(" ");
-        int position = 0;
-        String output = "";
+        Scanner scanner = new Scanner(System.in);
+        // Getting user input, split it to array
+        String[] input = scanner.nextLine().split(" ");
+        if (input == null || input.length == 0) {
+            System.out.print(" ");
+        } else {
+            // Creating second array to sort input
+            String[] sortingInput = new String[input.length];
 
-        for (String sting : input) {
-            for (int i = 0; i < sting.length(); i++) {
-                if (Character.isDigit(sting.charAt(i))) { // checks whether the index is an int or not
-                    if (position == 0) { // Sets the first word at the first position
-                        output = sting + " ";
-                        position = Character.getNumericValue(sting.charAt(i)); // parses the int-value of the index
-                    }
-                    if (Character.getNumericValue(sting.charAt(i)) > position) {
-                        output = output.concat(sting) + " ";
-                        position = Character.getNumericValue(sting.charAt(i)); // parses the int-value of the index
-                    }
-                    if (Character.getNumericValue(sting.charAt(i)) < position) {
-                        output = " " + sting.concat(output);
-                        position = Character.getNumericValue(sting.charAt(i)); // parses the int-value of the index
+            for (String sting : input) {
+                for (int i = 0; i < sting.length(); i++) {
+                    // Checks if character is digit
+                    if (Character.isDigit(sting.charAt(i))) {
+                        // Gets int-value of the character, puts it at the correct index of 2nd array
+                        sortingInput[Character.getNumericValue(sting.charAt(i)) - 1] = sting;
                     }
                 }
             }
+            for (String kabraxis : sortingInput) {
+                System.out.print(kabraxis + " ");
+            }
         }
-        System.out.println(output);
-
     }
 }
 /*
-is2, pos 2
-T
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
 
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
  */
-
