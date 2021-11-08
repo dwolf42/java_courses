1db86679e6129338d6416a67b8478aaec7e08e60
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-11-08 07:54:02 +0100

    get valid user coordinates, is valid turn

--- src/TicTacToeStage5Of5.java
+++ src/TicTacToeStage5Of5.java
@@ -46,11 +46,52 @@ public class TicTacToeStage5Of5 {
         System.out.println(horizontalBorder);
     }
 
+    // User's input of coordinates is a string that will be checked if it contains
+    // numbers and then the numbers will be translated to integers.
+    // Keeps asking for the correct input.
     public static int[] getValidCoordinates(char[][] board) {
         Scanner scanner = new Scanner(System.in);
+        // Only positive numbers are allowed.
         String pattern = "([-+])?\\d+";
-        printTurnMessage();
+        System.out.println("Enter the coordinates: ");
+        String[] coordinates = scanner.nextLine().split(" ");
 
+        // Loops as long as the input doesn't match the patten of only positive integers.
+        if (!coordinates[0].matches(pattern) || !coordinates[1].matches(pattern)) {
+            System.out.println("You should enter numbers!");
+            return getValidCoordinates(board);
+        }
+
+        // Translates string numbers into integers.
+        int[] numCoordinates = {Integer.parseInt(coordinates[0]), Integer.parseInt(coordinates[1])};
+
+        // In case the user enters coordinates bigger than 3 or smaller than 0 an error will be shown and
+        // the method will be reset and restarted.
+        if (numCoordinates[0] > 3 || numCoordinates[0] < 1 || numCoordinates[1] > 3 || numCoordinates[1] < 1) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            return getValidCoordinates(board);
+        }
+
+        // When user input is in range and a number, this checks whether it's valid or not.
+        if (!isValidTurn(board, numCoordinates)) {
+            return getValidCoordinates(board);
+        }
+
+        return numCoordinates;
+    }
+
+    // Coordinates provided by the user start counting from 1-3,
+    // while the counting in arrays start from 0-2.
+    // So the position on the board where the user wants to place their symbol has to be
+    // the user's entered coordinate - 1, like: input = 1 1 -> position = 0 0 on the board.
+    // That's why I subtract 1 in the if-condition here.
+    public static boolean isValidTurn(char[][] board, int[] getValidCoordinates) {
+        if (board[getValidCoordinates[0] - 1][getValidCoordinates[1] - 1] == '_') {
+            return true;
+        } else {
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        }
     }
 
     public static void printTurnMessage() {
