1cdb7d0067eb14754ee620bb630934be6ec880aa
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-09-28 22:30:12 +0200

    Using classes, initializing, instantiating, printing fields

---
+++ src/OOP/JetBrains/PatientDemo/Patient.java
@@ -0,0 +1,12 @@
+package OOP.JetBrains.PatientDemo;
+
+/**
+ * The class is a "blueprint" for patients
+ */
+class Patient {
+    String name;
+    int age;
+    float height;
+    String[] complaints;
+}
+
---
+++ src/OOP/JetBrains/PatientDemo/PatientDemo.java
@@ -0,0 +1,26 @@
+package OOP.JetBrains.PatientDemo;
+
+import java.util.Arrays;
+
+public class PatientDemo {
+    public static void main(String[] args) {
+        Patient kabraxis = new Patient();
+
+        kabraxis.name = "Kabraxis";
+        kabraxis.age = 25;
+        kabraxis.height = 1.70F;
+        kabraxis.complaints = new String[] {"Back Pain"};
+        kabraxis.complaints[4] = "back pain";
+
+        System.out.println(kabraxis.name + " " + kabraxis.age + " " + kabraxis.height + " " + Arrays.toString(kabraxis.complaints));
+
+        Patient alice = new Patient();
+
+        alice.name = "Alice";
+        alice.age = 22;
+        alice.height = 1.65F;
+
+        System.out.println(alice.name + " " + alice.age + " " + alice.height);
+
+    }
+}
