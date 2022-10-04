ee6028c06ce932e2f80472da4daf10be7a588dd8
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-04 19:15:43 +0200

    Exercise about static and instance methods

---
+++ src/OOP/JetBrains/InstanceMethods/Human.java
@@ -0,0 +1,18 @@
+package OOP.JetBrains.InstanceMethods;
+
+public class Human {
+    String name;
+    int age;
+
+    public static void averageWorking() {
+        System.out.println("An average human works 40 hours per week.");
+    }
+
+    public void work() {
+        System.out.println(this.name + " loves working!");
+    }
+
+    public void workTogetherWith(Human other) {
+        System.out.println(this.name + " loves working with " + other.name + '!');
+    }
+}
