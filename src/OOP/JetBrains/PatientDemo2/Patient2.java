79e76b424cae7822f19d02ebefbd4633277a59be
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-20 20:22:53 +0200

    add: new descriptive comment

--- src/OOP/JetBrains/PatientDemo2/Patient2.java
+++ src/OOP/JetBrains/PatientDemo2/Patient2.java
@@ -6,6 +6,7 @@ public class Patient2 {
     int age;
     float height;
 
+    // This is the constructor, it assigns the values to the fields
     public Patient2(String name, int age, float height) {
         this.name = name;
         this.age = age;
