a84088e0ca4e5536b27f7fb56eb77097d4c81842
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-25 00:42:10 +0200

    a simple clock

---
+++ src/OOP/JetBrains/Clock/Clock_Work.java
@@ -0,0 +1,9 @@
+package OOP.JetBrains.Clock;
+
+public class Clock_Work {
+    public static void main(String[] args) {
+        Clock clock = new Clock();
+        clock.next();
+    // Missing print statement
+    }
+}
