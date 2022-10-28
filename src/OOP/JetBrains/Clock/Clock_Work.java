18817c7a28151ea0164cb020521ec9c8d27f8fac
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-28 10:30:33 +0200

    feat: simulate 24h clock cycle in a fraction of real time
    Basically a demo of how Clock.java works in combination with Clock_Work.java.
    Each cycle a time will be printed to the user's screen, like a real digital clock.

--- src/OOP/JetBrains/Clock/Clock_Work.java
+++ src/OOP/JetBrains/Clock/Clock_Work.java
@@ -3,7 +3,12 @@ package OOP.JetBrains.Clock;
 public class Clock_Work {
     public static void main(String[] args) {
         Clock clock = new Clock();
-        clock.next();
-    // Missing print statement
+        // cycles are used to simulate 24h
+        int cycles = 0;
+        while (cycles < 720) {
+            clock.next();
+            System.out.printf("%d:%d%d%n", clock.hours, clock.minutes1, clock.minutes2);
+            cycles++;
+        }
     }
 }
