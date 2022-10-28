6cfeb1646e876a3afe74673254f12c716883de52
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-28 16:03:04 +0200

    feat: implement the static method whatTime()
    Hey, why not actually implementing a static method? Apart from the incorrect method name which suggests it was an instance method, instead of a static method :)

--- src/OOP/MyWork/Clock/Display.java
+++ src/OOP/MyWork/Clock/Display.java
@@ -2,25 +2,15 @@ package OOP.MyWork.Clock;
 
 public class Display {
 
-    /*
-     ***************************************************************
-     *  This part's purpose is educational only
-     *
-     *    public static void instanceMethod() {
-     *        System.out.println("Congrats, you've accessed the Instance Method :)");
-     *    }
-     ***************************************************************
-     */
 
-    String whatTime = "What time is it?";
+    public static void whatTime() {
+        System.out.println("What time is it?");
+    }
+
     String setHrs = "Set hours";
     String setTenMin = "Set ten minutes";
     String setOneMin = "Set one minutes";
     String theTime = "The time is";
-
-    public void whatTime() {
-        System.out.println(this.whatTime);
-    }
 
     public void setHrs() {
         System.out.print(this.setHrs);
