9c39f2e8365de490eed76e5be2ce269e20865da5
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-28 11:11:27 +0200

    feat: ask to set current time in hours, ten and one minutes, sleep is 3 sec
    User gets asked to set their current time, clock will be simulated in 3 seconds ticks.

--- src/OOP/MyWork/Clock/Clock_Work.java
+++ src/OOP/MyWork/Clock/Clock_Work.java
@@ -1,15 +1,24 @@
 package OOP.MyWork.Clock;
 import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 public class Clock_Work {
     public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
         Clock clock = new Clock();
+
+        System.out.println("What time is it?");
+        System.out.print("\nSet hours: ");
+        clock.hours = scanner.nextInt();
+        System.out.print("\nSet the ten minutes: ");
+        clock.minutes1 = scanner.nextInt();
+        System.out.print("\nSet one minutes: ");
+        clock.minutes2 = scanner.nextInt();
 
         while (true) {
             clock.next();
             System.out.printf("%d:%d%d%n", clock.hours, clock.minutes1, clock.minutes2);
-            TimeUnit.SECONDS.sleep(5);
-
+            TimeUnit.SECONDS.sleep(3);
         }
     }
 }
