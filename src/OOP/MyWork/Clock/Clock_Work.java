555c359ea25d41950d3f2b9cb6980fb52ba5b891
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-31 10:00:39 +0100

    refactor: clarify variable names, introduce getters

--- src/OOP/MyWork/Clock/Clock_Work.java
+++ src/OOP/MyWork/Clock/Clock_Work.java
@@ -13,27 +13,27 @@ public class Clock_Work {
          *  This is used to access the static methods inside the class Display
          * To invoke a static method we don't need to create an object. We just call the method with the class name.
          */
-        Display.whatTime();
+        Display.printWhatTime();
 
         /**
          * These are used to access the instance methods of the object display,
          * which is an instance of the class Display
          */
-        display.setHrs();
+        System.out.print(display.getPrintSetHours());
         clock.hours = scanner.nextInt();
-        display.setTenMin();
+        System.out.print(display.getPrintSetTenMinutes());
         clock.minutes1 = scanner.nextInt();
-        display.setOneMin();
+        System.out.print(display.getPrintSetOneMinutes());
         clock.minutes2 = scanner.nextInt();
 
         System.out.println("");
-        display.currentTime();
+        System.out.print(display.getPrintTheTime());
         System.out.printf("%d:%d%d%n", clock.hours, clock.minutes1, clock.minutes2);
         TimeUnit.SECONDS.sleep(3);
 
         while (true) {
             clock.next();
-            display.currentTime();
+            System.out.print(display.getPrintTheTime());
             System.out.printf("%d:%d%d%n", clock.hours, clock.minutes1, clock.minutes2);
             TimeUnit.SECONDS.sleep(3);
         }
