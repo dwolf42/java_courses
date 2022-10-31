7716c8ea60dbf36775d1039d5465efaa05baaeb6
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-31 10:20:57 +0100

    refactor: introduce getters and setters, clarify variables by this-keyword
    In order to practice getters and setters, even if I haven't learned about them in my course yet, I wanted to utilize them.

--- src/OOP/MyWork/Clock/Clock.java
+++ src/OOP/MyWork/Clock/Clock.java
@@ -7,25 +7,49 @@ public class Clock {
     private static final int MIN_MINUTES = 0;
     private static final int MIN_HOURS = 1;
 
-    int hours = MAX_HOURS;
-    int minutes1 = MIN_MINUTES;
-    int minutes2 = MIN_MINUTES;
+    private int hours = MAX_HOURS;
+    private int minutes1 = MIN_MINUTES;
+    private int minutes2 = MIN_MINUTES;
 
     void next() {
-        minutes2++;
-        if (minutes2 > MAX_MINUTES2) {
-            minutes2 = MIN_MINUTES;
-            minutes1++;
+        this.minutes2++;
+        if (this.minutes2 > MAX_MINUTES2) {
+            this.minutes2 = MIN_MINUTES;
+            this.minutes1++;
         }
 
-        if (minutes1 > MAX_MINUTES1) {
-            minutes1 = MIN_MINUTES;
-            hours++;
+        if (this.minutes1 > MAX_MINUTES1) {
+            this.minutes1 = MIN_MINUTES;
+            this.hours++;
         }
 
-        if (hours > MAX_HOURS) {
-            hours = MIN_HOURS;
+        if (this.hours > MAX_HOURS) {
+            this.hours = MIN_HOURS;
         }
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void setMinutes1(int minutes1) {
+        this.minutes1 = minutes1;
+    }
+
+    public void setMinutes2(int minutes2) {
+        this.minutes2 = minutes2;
+    }
+
+    public int getHours() {
+        return this.hours;
+    }
+
+    public int getMinutes1() {
+        return this.minutes1;
+    }
+
+    public int getMinutes2() {
+        return this.minutes2;
     }
 
 }
