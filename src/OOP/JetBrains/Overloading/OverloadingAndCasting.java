f607085c03dba7196f906cb9e8a89876b6ef4b1b
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-11-01 12:49:41 +0100

    feat: implement an example about method overloading and type casting
    In this example we see what will happen if run this code, having all methods active. In comparison, we will see that Java sees 100 as an integer literal. If we comment-out print(int a) Java will handle 100 as the next safely implicit cast, which is long and double respective. When commenting out every save implicit cast, and we try to print 100 Java won't compile. So we have to explicitly cast 100 to short.

---
+++ src/OOP/JetBrains/Overloading/OverloadingAndCasting.java
@@ -0,0 +1,32 @@
+package OOP.JetBrains.Overloading;
+
+public class OverloadingAndCasting {
+
+        public static void print(short a) {
+            System.out.println("short arg: " + a);
+        }
+
+        public static void print(int a) {
+            System.out.println("int arg: " + a);
+        }
+
+        public static void print(long a) {
+            System.out.println("long arg: " + a);
+        }
+
+        public static void print(double a) {
+            System.out.println("double arg: " + a);
+        }
+
+        public static void main(String[] args) {
+            print(100);
+        }
+
+    }
+
+    /*
+    When we pass some value to the method, the compiler does not evaluate it. All that is known is that it is an
+    integer literal, so it has the integer type. In our case, 100 is treated as an int by default and JVM doesn't know
+    if the passed value can be cast to short safely. So, the only way to pass the short argument is by
+    casting the value explicitly: print((short)100);
+    */
