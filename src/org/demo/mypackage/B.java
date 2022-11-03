450e2d7f135f4e57ecd7bd41ba6c47bb00efa562
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-11-03 12:50:55 +0100

    feat: add examples for short name and full name usage

--- src/org/demo/mypackage/B.java
+++ src/org/demo/mypackage/B.java
@@ -1,8 +1,9 @@
 package org.demo.mypackage;
 
 public class B {
-    C c1 = new C();
-    C c2 = new org.demo.mypackage.C();
+    C c1 = new C(); // Short name usage
+    C c2 = new org.demo.mypackage.C(); // Well, yes, it works, too
+    org.demo.mypackage.C c3 = new org.demo.mypackage.C(); // Full name usage
     // non-functional
 //    mypackage.C c3 = new org.demo.mypackage.C();
 //    A a = new A();
