bdba39c23cf18e532bfcc14c98f16ac4da174e36
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-11-17 11:55:05 +0100

    feature: add readme, getters and setters

---
+++ src/com/github/kabraxis/oop/jet_brains/getters_settes/gettingfullname/User.java
@@ -0,0 +1,28 @@
+package com.github.kabraxis.oop.jet_brains.getters_settes.gettingfullname;
+
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public User() {
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName != null) {
+            this.firstName = firstName.trim();
+        }
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName != null) {
+            this.lastName = lastName.trim();
+        }
+    }
+
+    public String getFullName() {
+        return this.firstName.isEmpty() && this.lastName.isEmpty() ? "Unknown" :
+                this.firstName.trim().concat(" ").concat(this.lastName.trim());
+    }
+}
