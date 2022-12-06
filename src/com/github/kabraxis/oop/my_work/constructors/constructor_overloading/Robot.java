d362addad95a0bf9f27c5c6fd89ef30a768bae31
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-12-06 10:04:57 +0100

    feature: showcase constructor overloading

---
+++ src/com/github/kabraxis/oop/my_work/constructors/constructor_overloading/Robot.java
@@ -0,0 +1,24 @@
+package com.github.kabraxis.oop.my_work.constructors.constructor_overloading;
+
+public class Robot {
+    String name;
+    String model;
+    int lifetime;
+
+    Robot() {
+        this.name = "Anonymous";
+        this.model = "Unknown";
+    }
+
+    public Robot(String name, String model) {
+        this(name, model, 20);
+    }
+
+    public Robot(String name, String model, int lifetime) {
+        this.name = name;
+        this.model = model;
+        this.lifetime = lifetime;
+        System.out.println("The third constructor is invoked");
+    }
+
+}
---
+++ src/com/github/kabraxis/oop/my_work/constructors/constructor_overloading/RobotFactory.java
@@ -0,0 +1,9 @@
+package com.github.kabraxis.oop.my_work.constructors.constructor_overloading;
+
+public class RobotFactory {
+    public static void main(String[] args) {
+        Robot anonymous = new Robot();
+        Robot harry = new Robot("Harry", "X3H-N9");
+
+    }
+}
