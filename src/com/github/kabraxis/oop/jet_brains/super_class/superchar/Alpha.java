99c4415d9bda556b8f41fce77ae3b0dfeef317b1
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-12-06 14:51:16 +0100

    feature: showcase usage of super-keyword

---
+++ src/com/github/kabraxis/oop/jet_brains/super_class/superchar/Alpha.java
@@ -0,0 +1,28 @@
+package com.github.kabraxis.oop.jet_brains.super_class.superchar;
+
+class Alpha {
+
+    protected int n = 10;
+    protected String s = "abc";
+    protected char ch = 'q';
+
+    public Alpha(int n) {
+        this.n = n;
+    }
+}
+
+class Beta extends Alpha {
+    protected int n = 20;
+    protected String s = "str";
+    protected char ch = 'z';
+
+    public Beta(int n, String s, char ch) {
+        super(n);
+        this.s = s;
+        super.ch = ch;
+    }
+
+    public void print() {
+        System.out.println(super.n + " " + super.s + " " + ch);
+    }
+}
