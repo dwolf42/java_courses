5e8c395f70f46c235b479b418380178f91b09937
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-12-12 19:58:29 +0100

    refactor: rename package

--- src/com/github/kabraxis/oop/jet_brains/objects/reference/ObjectReferences.java
+++ src/com/github/kabraxis/oop/jet_brains/objects/reference/ObjectReferences.java
@@ -1,6 +1,6 @@
 // Objects point to the same reference always share the same value
 
-package com.github.kabraxis.oop.jet_brains.objects.references;
+package com.github.kabraxis.oop.jet_brains.objects.reference;
 
 public class ObjectReferences {
     public static void main(String[] args) {
