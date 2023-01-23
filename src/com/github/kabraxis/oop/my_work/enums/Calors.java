82aea0c559b11a33f4451be9c2f9d35100155bf0
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-01-23 12:33:57 +0100

    feature: introduction to enums

---
+++ src/com/github/kabraxis/oop/my_work/enums/Calors.java
@@ -0,0 +1,10 @@
+package com.github.kabraxis.oop.my_work.enums;
+
+public class Calors {
+    private Calors() {
+    }
+
+    public static final Calors RED = new Calors();
+    public static final Calors AMBER = new Calors();
+    public static final Calors GREEN = new Calors();
+}
---
+++ src/com/github/kabraxis/oop/my_work/enums/Color.java
@@ -0,0 +1,7 @@
+package com.github.kabraxis.oop.my_work.enums;
+
+public enum Color {
+    RED,
+    AMBER,
+    GREEN;
+}
