8712fabc8b27a4dbe56d3249ff8b2d41f9cc6dc3
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-03-23 20:58:20 +0100

    style: comments, spacing

--- src/rpg/Monster.java
+++ src/rpg/Monster.java
@@ -5,18 +5,19 @@ public class Monster {
     private int healthPoints;
     private int strength;
 
+    // Very basic attacking
     public void attack(Character character, Monster monster) {
         // Reduces HP of attacked character, based on monsters strength
         character.setHealthPoints(character.getHealthPoints() - monster.getStrength());
         System.out.printf("Your HP: %d\n", character.getHealthPoints());
     }
 
+    // The section below contains boilerplate code like the constructor and all the getters and setters
     public Monster(String name, int healthPoints, int strength) {
         this.name = name;
         this.healthPoints = healthPoints;
         this.strength = strength;
     }
-
 
     public String getName() {
         return name;
