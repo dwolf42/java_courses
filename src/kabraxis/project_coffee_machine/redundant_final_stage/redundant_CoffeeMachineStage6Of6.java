fda6c393dca466b22bdc16ae726c948bf30adff0
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-03-28 02:06:19 +0200

    files are redundant and not supported anymore!

---
+++ src/kabraxis/project_coffee_machine/redundant_final_stage/redundant_CoffeeMachineStage6Of6.java
@@ -0,0 +1,16 @@
+package kabraxis.project_coffee_machine.redundant_final_stage;
+
+public class redundant_CoffeeMachineStage6Of6 {
+    public static void main(String[] args) {
+        redundant_CoffeeMachine deLuvos = new redundant_CoffeeMachine();
+        deLuvos.processor();
+    }
+
+
+
+
+
+
+
+}
+
---
+++ src/kabraxis/project_coffee_machine/redundant_final_stage/redundant_MachineState.java
@@ -0,0 +1,10 @@
+package kabraxis.project_coffee_machine.redundant_final_stage;
+
+enum redundant_MachineState {
+    SELECT,
+    BUY,
+    FILL_WATER,
+    FILL_MILK,
+    FILL_BEANS,
+    FILL_CUPS
+}
