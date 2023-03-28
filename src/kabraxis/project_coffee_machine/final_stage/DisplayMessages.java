564a3234f2b62bf0eb2beb7894c05765edf4c0e7
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-03-28 17:35:16 +0200

    feature: start implementing constructor

--- src/kabraxis/project_coffee_machine/final_stage/DisplayMessages.java
+++ src/kabraxis/project_coffee_machine/final_stage/DisplayMessages.java
@@ -8,11 +8,18 @@ public class DisplayMessages {
     public final String BUY_MESSAGE = "What do you want to buy?" +
             "\nPlease enter: '1' - for espresso, '2' - for latte, " +
             "'3' - for cappuccino, 'back' - for main menu:";
+    public final String MISSING_ITEM = String.format("Sorry, not enough %s!%n%n",);
+    public final String TAKE_MESSAGE = String.format("%nI gave you $%d%n",deLuvos);
     public final String SET_WATER = "Input how many ml of water you want to add: ";
     public final String SET_MILK = "Input how many ml of milk you want to add: ";
     public final String SET_BEANS = "Input how many grams of coffee beans you want to add: ";
     public final String SET_CUPS = "Input how many disposable cups of coffee you want to add: ";
     public final String CONFIRM_ORDER = "I have enough resources, making you a coffee!";
 
+    public DisplayMessages() {
 
+    }
+    public static DisplayMessages getMessagesObject() {
+        return messages;
+    }
 }
