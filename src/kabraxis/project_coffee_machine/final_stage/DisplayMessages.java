6af234a932bfde0dd66669eb9904f29e9171b56e
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-03-30 01:56:47 +0200

    feature: handle mainly all messages

--- src/kabraxis/project_coffee_machine/final_stage/DisplayMessages.java
+++ src/kabraxis/project_coffee_machine/final_stage/DisplayMessages.java
@@ -3,13 +3,12 @@ package kabraxis.project_coffee_machine.final_stage;
 public class DisplayMessages {
     public final String ERROR_SELECT_INPUT = "Error! Invalid input.";
     public final String ERROR_FILL_INPUT = "You may only add amounts of 0 and above";
-    public final String ERROR_EXIT = "Turning off...";
-    public final String SELECT_MESSAGE = "Please enter an action \"buy, fill, take, remaining, exit\": ";
-    public final String BUY_MESSAGE = "What do you want to buy?" +
-            "\nPlease enter: '1' - for espresso, '2' - for latte, " +
-            "'3' - for cappuccino, 'back' - for main menu:";
-    public final String MISSING_ITEM = String.format("Sorry, not enough %s!%n%n",);
-    public final String TAKE_MESSAGE = String.format("%nI gave you $%d%n",deLuvos);
+    public final String EXIT_MESSAGE = "Turning off...";
+    public final String SELECT_MESSAGE = "Write action (buy, fill, take, remaining, exit):";
+    public final String BUY_MESSAGE = "What do you want to buy? " +
+            "1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:";
+    public final String MISSING_ITEM = "Sorry, not enough ";
+    public final String TAKE_MESSAGE = "\nI gave you $";
     public final String SET_WATER = "Input how many ml of water you want to add: ";
     public final String SET_MILK = "Input how many ml of milk you want to add: ";
     public final String SET_BEANS = "Input how many grams of coffee beans you want to add: ";
