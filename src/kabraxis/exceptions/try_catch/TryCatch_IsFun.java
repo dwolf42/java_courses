f928d1208bde660de10e163c68b14561042f001b
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-04-07 15:28:13 +0200

    feature: rework the program

--- src/kabraxis/exceptions/try_catch/TryCatch_IsFun.java
+++ src/kabraxis/exceptions/try_catch/TryCatch_IsFun.java
@@ -1,17 +1,24 @@
 package kabraxis.exceptions.try_catch;
 
+import java.util.Scanner;
+
 public class TryCatch_IsFun {
     public static void main(String[] args) {
-        System.out.println("before the try-catch block"); // it will be printed
+        catchout();
+    }
+
+    public static void catchout() {
+        Scanner scanner = new Scanner(System.in);
+        tryout(scanner.nextLine());
+    }
+
+    public static void tryout(String str) {
         try {
-            System.out.println("inside the try block before an exception"); // it will be printed
-
-            System.out.println(2/0); // it throws ArithmeticException
-
-            System.out.println("inside the try block after exception"); // it won't be printed
+            System.out.println(Integer.parseInt(str));
         } catch (NumberFormatException e) {
-            System.out.println("Division by zero!"); // it will be printed
+            System.out.println("You may only input numbers");
+            catchout();
         }
-        System.out.println("after the try-catch block"); // it will be printed
+        System.out.println("End of tryout!");
     }
 }
