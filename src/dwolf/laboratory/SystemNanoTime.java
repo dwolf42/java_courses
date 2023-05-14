1cd5d391aefe83ddfe185ef195fef736382be354
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-05-14 17:55:30 +0200

    feature: simple implementation of System.nanoTime

---
+++ src/dwolf/laboratory/SystemNanoTime.java
@@ -0,0 +1,8 @@
+package dwolf.laboratory;
+
+public class SystemNanoTime {
+    public static void main(String[] args) {
+        long pseudoRandomNumber = System.nanoTime();
+        System.out.println(pseudoRandomNumber);
+    }
+}
