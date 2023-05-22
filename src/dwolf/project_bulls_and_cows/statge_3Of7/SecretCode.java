891bbdb4647ca7ae0d6cb30f8c1bff9012428f7f
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-05-22 16:55:56 +0200

    feature: generate pseudo-random code
    SecretCode is able to generate a pseudo-random code of given length, but
    if the code is seed is 112233, and given length 5, the result will be
    321 (code is reversed, this is intended!).

---
+++ src/dwolf/project_bulls_and_cows/statge_3Of7/SecretCode.java
@@ -0,0 +1,64 @@
+package dwolf.project_bulls_and_cows.statge_3Of7;
+
+public class SecretCode {
+    private int codeGivenLength;
+    private StringBuilder randomCode;
+
+    public String getNewSecretCode() {
+        setCodeGivenLength();
+        randomCode = new StringBuilder(getPseudoRandomNumber());
+        reverseCode();
+        deleteLeadingZero();
+        // TODO: I need to make sure that the code has the desired length of individual digits if the generated code was e.g. 332211
+        if (randomCode.length() > getCodeGivenLength()) {
+            shortenCode();
+        }
+
+        return "0";
+    }
+
+    public void reverseCode() {
+        System.out.println("pre reverse " + randomCode);
+        randomCode.reverse();
+        System.out.println("post reverse " + randomCode);
+    }
+
+    private void deleteLeadingZero() {
+        while (randomCode.charAt(0) == '0') {
+            randomCode.delete(0, 1);
+        }
+        System.out.println("post delete leading zero/s: " + randomCode);
+        deleteDuplicates();
+    }
+
+    private void deleteDuplicates() {
+        for (int i = 0; i < randomCode.length(); i++) {
+            for (int j = i + 1; j < randomCode.length(); j++) {
+                if (randomCode.charAt(i) == randomCode.charAt(j)) {
+                    randomCode.deleteCharAt(j);
+                }
+            }
+        }
+
+        System.out.println("post duplicates elimination: " + randomCode);
+    }
+
+    private void shortenCode() {
+        randomCode.delete(getCodeGivenLength(), randomCode.length());
+        System.out.println("post shorten code to fit codeGivenLength: " + randomCode);
+    }
+
+    private void setCodeGivenLength() {
+        codeGivenLength = Integer.parseInt(Main.getInput());
+    }
+
+    private int getCodeGivenLength() {
+        return codeGivenLength;
+    }
+
+    private String getPseudoRandomNumber() {
+//        return Long.toString(System.nanoTime());
+        return Long.toString(112233L);
+    }
+
+}
