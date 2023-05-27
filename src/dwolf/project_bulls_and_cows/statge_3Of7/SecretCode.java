ae71a8b48e69b834462832908a72b69280fd55a8
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-05-27 19:03:08 +0200

    refactor: establish reusable methods

--- src/dwolf/project_bulls_and_cows/statge_3Of7/SecretCode.java
+++ src/dwolf/project_bulls_and_cows/statge_3Of7/SecretCode.java
@@ -8,38 +8,38 @@ public class SecretCode {
     public String getNewSecretCode() {
         setCodeGivenLength();
         randomCode = new StringBuilder(getPseudoRandomNumber());
-        reverseCode();
-        deleteLeadingZero();
+        reverseCode(randomCode);
+        deleteLeadingZero(randomCode);
+        deleteDuplicates(randomCode);
 
         // In case the code is too long
         if (randomCode.length() > getCodeGivenLength()) {
-            shortenCode();
+            shortenCode(randomCode);
         }
 
         // In case the code is too short
-        while (randomCode.length() < getCodeGivenLength()) {
-            extendCode();
+        if (randomCode.length() < getCodeGivenLength()) {
+            extendCode(randomCode);
         }
 
-        return "0";
+        return randomCode.toString();
     }
 
-    public void reverseCode() {
+    public void reverseCode(StringBuilder randomCode) {
         System.out.println("pre reverse " + randomCode);
         randomCode.reverse();
         System.out.println("post reverse " + randomCode);
     }
 
     // Is the first digit of the number a zero? It will be deleted.
-    private void deleteLeadingZero() {
+    private void deleteLeadingZero(StringBuilder randomCode) {
         while (randomCode.charAt(0) == '0') {
             randomCode.delete(0, 1);
         }
         System.out.println("post delete leading zero/s: " + randomCode);
-        deleteDuplicates();
     }
 
-    private void deleteDuplicates() {
+    private void deleteDuplicates(StringBuilder randomCode) {
         for (int i = 0; i < randomCode.length(); i++) {
             for (int j = i + 1; j < randomCode.length(); j++) {
                 if (randomCode.charAt(i) == randomCode.charAt(j)) {
@@ -51,15 +51,15 @@ public class SecretCode {
         System.out.println("post duplicates elimination: " + randomCode);
     }
 
-    private void shortenCode() {
+    private void shortenCode(StringBuilder randomCode) {
         randomCode.delete(getCodeGivenLength(), randomCode.length());
         System.out.println("post shorten code to fit codeGivenLength: " + randomCode);
     }
 
-    private void extendCode() {
+    private void extendCode(StringBuilder randomCode) {
         int extendFor = getCodeGivenLength() - randomCode.length();
         spareRandomCode = new StringBuilder(getPseudoRandomNumber());
-
+        spareRandomCode.reverse();
     }
 
     private void setCodeGivenLength() {
