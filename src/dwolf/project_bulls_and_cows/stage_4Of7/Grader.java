ab422e13cd4895eb66d970ed223e63c4ccfe9546
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-05-31 19:30:31 +0200

    docs: add descriptive comments if necessary
    Today, only God and I know what I did, now tomorrow we both will know ;)

--- src/dwolf/project_bulls_and_cows/stage_4Of7/Grader.java
+++ src/dwolf/project_bulls_and_cows/stage_4Of7/Grader.java
@@ -4,6 +4,8 @@ class Grader {
     private int bulls = 0;
     private int cows = 0;
 
+    // A bull is, if the guessed number is at the same position of the generated number.
+    // Whereas a cow is, if the guessed number is part of the generated number, but only at the wrong place.
     protected int[] countCows(char[] secret, char[] guess) {
         for (int i = 0; i < secret.length; i++) {
             for (int j = 0; j < guess.length; j++) {
@@ -16,6 +18,11 @@ class Grader {
                 }
             }
         }
+
+        // Returns a new array, index 0 equals to bulls, index 1 equals to cows detected in user's guess.
+        // The evaluation for cows is: if cows >= bulls, then the value of cows is return cows - bulls, else return
+        // the value of cows directly.
+        // This is required, so bulls won't get count as cows.
         return new int[]{bulls, cows >= bulls ? cows - bulls : cows};
     }
 
--- src/dwolf/project_bulls_and_cows/stage_4Of7/Main.java
+++ src/dwolf/project_bulls_and_cows/stage_4Of7/Main.java
@@ -39,19 +39,29 @@ import java.util.Scanner;
  */
 
 public class Main {
+    // Game starts at turn 1
     static int turn = 1;
+
+    // The amount of bulls and cows are stored in an array. These are the indices that describe where in animalFold[]
+    // the corresponding values are stored.
     static final int indexBulls = 0;
     static final int indexCows = 1;
+
+    // How many bulls are required to win is determined by the length of the code the user wants to guess.
     static int winBulls;
+
     static String secretCode;
 
     public static void main(String[] args) {
         initialize();
     }
 
+    // Starts the game, by getting a new secret code and setting win condition.
     protected static void initialize() {
-        // Anonymous object, since it's only used here
         System.out.println("Please, enter the secret code's length:");
+        // Anonymous object, since it's only used here.
+        // String input ins parsed to int, since SecretCode().getSecretCode() requires int, for
+        // further detail: See getInput()
         secretCode = new SecretCode().getSecretCode(Integer.parseInt(getInput()));
         winBulls = secretCode.length();
         System.out.println("Okay, let's start the game!");
@@ -59,14 +69,21 @@ public class Main {
         run();
     }
 
+    // Manages the game logic. Generated secret code and the users guess are pre-processed here, before Grader()
+    // evaluates these against each other.
+    // This method could have been broken down further, but I wanted to keep things simple.
     protected static void run() {
         System.out.printf("Turn %d: \n",
                 turn);
 
+        // Evaluation of secret code vs. guess is done by string processing, which I found easier to implement.
         char[] secret = secretCode.toCharArray();
         char[] guess = getInput().toCharArray();
+
         // Anonymous object, since it's only used here.
+        // animalFold[] is where the code saves how many bulls and cows are correctly guesses.
         int[] animalFold = new Grader().countCows(secret, guess);
+
         if (animalFold[indexBulls] == winBulls) {
             System.out.printf("Grade: %d bulls.\nCongrats! The secret code is %s.",
                     animalFold[indexBulls], secretCode);
@@ -96,6 +113,9 @@ public class Main {
         }
     }
 
+    // I wanted to have only one method responsible to get user input. As the whole evaluation of the secret code vs.
+    // guessed code is done by string processing, all inputs are taken as string.
+    // WARNING: There is no input safety, in order to save time by focusing on the main task objectives.
     protected static String getInput() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
