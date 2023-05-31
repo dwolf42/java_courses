f4984a2e626a76f16847861c6a89b0a2f17f0287
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-05-31 23:31:14 +0200

    feat: demonstrate Random()

---
+++ src/random/random_random_class.java
@@ -0,0 +1,25 @@
+package random;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * ~~Class description~~
+ * This class generates a new random number of a user-given length.
+ * In the process, System.nanoTime is utilized to generate a pseudo-random number as a string.
+ * The random number's length must not exceed a length of 10, since longer numbers would lack uniqueness.
+ */
+
+
+public class random_random_class {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println("Double: " + random.nextDouble());
+        System.out.println("Boolean: " + random.nextBoolean());
+        byte[] bytes = new byte[10];
+        random.nextBytes(bytes);
+        // Print the byte array as a string
+        System.out.println("Byte Array: " + Arrays.toString(bytes));
+    }
+}
