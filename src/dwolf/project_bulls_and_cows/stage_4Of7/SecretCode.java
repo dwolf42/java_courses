ab8ec0b9f193f13dad566c5685091380078b02dd
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-05-31 18:07:48 +0200

    docs: move parts of documentation to Main class

--- src/dwolf/project_bulls_and_cows/stage_4Of7/SecretCode.java
+++ src/dwolf/project_bulls_and_cows/stage_4Of7/SecretCode.java
@@ -5,43 +5,6 @@ package dwolf.project_bulls_and_cows.stage_4Of7;
  * This class has the task of generating a new secret code of a user-given length, which than has to be guessed in the
  * Bulls and Cows game. In the process, System.nanoTime is utilized to generate a pseudo-random number as a string.
  * The secret code's length must not exceed a length of 10, since longer numbers would lack uniqueness of numbers.
- * The code written here is a pre-requirement for the further game development. For further detail, please read
- * the task description.
- * <p>
- * <p>
- * ~~Task description~~
- * In this stage, you should combine all the previous parts into a simple playable version of the "Bulls and Cows" game.
- * First, prompt the player to input the length of the secret code. The length will determine the difficulty level for
- * the current game session. The program should generate a secret code of the given length. Remember that it should
- * consist of unique numbers.
- * <p>
- * Then, the game starts and the program prompts the player to guess the code. When the player inputs a number, the
- * program should grade it in bulls and cows. The game goes on until the code is guessed, that is, until the number of
- * bulls is equal to the number of digits in the code. When the game ends, the program should finish its execution.
- * <p>
- * <p>
- * ~~Objective~~
- * In this stage, your program should:
- * 1. Ask for the length of the secret code and then generate the code.
- * 2. Wait for the user input.
- * 3. Grade the guessing attempt in bulls and cows.
- * 4. If the secret code has been guessed, the program stops; otherwise, return to the second step.
- * <p>
- * <p>
- * ~~Examples~~
- * Please, enter the secret code's length:
- * > 4
- * Okay, let's start a game!
- * Turn 1:
- * > 1234
- * Grade: 1 bull and 1 cow
- * Turn 2:
- * > 7354
- * Grade: 2 bulls and 1 cow
- * Turn 3:
- * > 9374
- * Grade: 4 bulls
- * Congratulations! You guessed the secret code.
  */
 
 class SecretCode {
