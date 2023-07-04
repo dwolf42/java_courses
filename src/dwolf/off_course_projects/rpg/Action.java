ad90906c4bc28b905d7d417a4b6685603eb43e7a
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-07-04 23:40:20 +0200

    feat: add enums with state

--- src/dwolf/off_course_projects/rpg/Action.java
+++ src/dwolf/off_course_projects/rpg/Action.java
@@ -1,4 +1,26 @@
 package dwolf.off_course_projects.rpg;
 
-public enum Action {
+enum Action {
+    ATTACK(1),
+    DEFEND(2),
+    CAST_SPELL(3);
+
+    private final int number;
+
+    Action(int number) {
+        this.number = number;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    static Action fromInt(int i) {
+        for (Action action : Action.values()) {
+            if (action.getNumber() == i) {
+                return action;
+            }
+        }
+        return null;
+    }
 }
