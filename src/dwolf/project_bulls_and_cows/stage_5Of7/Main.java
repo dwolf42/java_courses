85fec5f5e528f915015be68b27fd1328f3c8b0f6
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-07-30 00:09:13 +0200

    docs: update documentation

--- src/dwolf/project_bulls_and_cows/stage_5Of7/Main.java
+++ src/dwolf/project_bulls_and_cows/stage_5Of7/Main.java
@@ -12,25 +12,23 @@ import java.util.Scanner;
  * <p>
  * <p>
  * ~~Task description~~
- * In this stage, you should combine all the previous parts into a simple playable version of the "Bulls and Cows" game.
- * First, prompt the player to input the length of the secret code. The length will determine the difficulty level for
- * the current game session. The program should generate a secret code of the given length. Remember that it should
- * consist of unique numbers.
+ * The algorithm suggested for generating the secret code in the previous stage was really a “reinvention of the wheel”.
+ * Java already has the tools for generating random numbers! Research some common pseudo-random generation methods such
+ * as Math.random() and other methods from the Random class.
+ * Choose the method you like and use it to rewrite the secret code generation.
  * <p>
- * Then, the game starts and the program prompts the player to guess the code. When the player inputs a number, the
- * program should grade it in bulls and cows. The game goes on until the code is guessed, that is, until the number of
- * bulls is equal to the number of digits in the code. When the game ends, the program should finish its execution.
+ * Nothing else is supposed to change at this stage: the program asks for the length, generates a secret code,
+ * and then receives and grades the attempts until the code is guessed. Your task here is to rewrite the code generator
+ * without breaking the existing code.
  * <p>
  * <p>
  * ~~Objective~~
- * In this stage, your program should:
- * 1. Ask for the length of the secret code and then generate the code.
- * 2. Wait for the user input.
- * 3. Grade the guessing attempt in bulls and cows.
- * 4. If the secret code has been guessed, the program stops; otherwise, return to the second step.
+ * In this stage, rewrite the secret code generator using a suitable Java method.
  * <p>
  * <p>
- * ~~Examples~~
+ * ~~Example~~
+ * The greater-than symbol followed by a space > represents the user input. Note that it's not part of the input.
+ * <p>
  * Please, enter the secret code's length:
  * > 4
  * Okay, let's start a game!
