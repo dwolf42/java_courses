99e5e6660f11ea52b3773920117730a1eb968928
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-08-15 17:53:25 +0200

    feat: JUnit test for random int & get secret code

---
+++ src/dwolf/project_bulls_and_cows/stage_6Of7/SecretCodeGeneratorTest.java
@@ -0,0 +1,28 @@
+package dwolf.project_bulls_and_cows.stage_6Of7;
+
+import org.junit.jupiter.api.Test;
+
+class SecretCodeGeneratorTest {
+    @Test
+    void testGetRandomIntFromSeed() {
+        final char[] alphanumeric = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z'};
+
+        SecretCodeGenerator gen = new SecretCodeGenerator();
+        // 0 1 2
+        for (int i = 0; i < 45; i++) {
+            System.out.println(alphanumeric[gen.getRandomIntFromSeed(16)]);
+        }
+    }
+
+    @Test
+    void testGetSecretCode() {
+        SecretCodeGenerator gen = new SecretCodeGenerator();
+        System.out.println(gen.getSecretCode(10));
+
+    }
+
+
+}
