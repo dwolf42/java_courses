5dff4f8c59365d55f4eca903fb2ba365ed23fdbe
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-08-16 17:17:53 +0200

    feat: prepare code for tests, and use of string
    There are now getters for bulls and cows variables. Also, the secretCode and guess are processed as strings.

--- src/dwolf/project_bulls_and_cows/stage_6Of7/Grader.java
+++ src/dwolf/project_bulls_and_cows/stage_6Of7/Grader.java
@@ -4,19 +4,20 @@ package dwolf.project_bulls_and_cows.stage_6Of7;
  * Secret code vs. guessed code comparison is the only task for this class.
  */
 class Grader {
-    private int bulls = 0;
-    private int cows = 0;
+
+    private int bulls;
+    private int cows;
+
 
     // A bull is, if the guessed number is at the same position of the generated number.
     // Whereas a cow is, if the guessed number is part of the generated number, but only at the wrong place.
-    protected int[] countCows(char[] secret, char[] guess) {
-        for (int i = 0; i < secret.length; i++) {
-            for (int j = 0; j < guess.length; j++) {
-                if (secret[i] == guess[j] && i == j) {
+    protected void gradeGuess(String secretCode, String guess) {
+        for (int i = 0; i < secretCode.length(); i++) {
+            for (int j = 0; j < guess.length(); j++) {
+                if (secretCode.charAt(i) == guess.charAt(j) && i == j) {
                     bulls++;
                 }
-
-                if (secret[i] == guess[j]) {
+                if (secretCode.charAt(i) == guess.charAt(j)) {
                     cows++;
                 }
             }
@@ -26,7 +27,17 @@ class Grader {
         // The evaluation for cows is: if cows >= bulls, then the value of cows is return cows - bulls, else return
         // the value of cows directly.
         // This is required, so bulls won't get count as cows.
-        return new int[]{bulls, cows >= bulls ? cows - bulls : cows};
     }
 
+    public int getBulls() {
+        return bulls;
+    }
+
+    public int getCows() {
+        return cows;
+    }
+
+    protected int[] countCows(char[] secretCode, char[] guess) {
+        return new int[]{bulls, cows >= bulls ? cows - bulls : cows};
+    }
 }
