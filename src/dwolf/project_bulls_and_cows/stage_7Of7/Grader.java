0d4b47b7e17f28c74d9a6c03fb29535996e6a901
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-08-18 00:38:18 +0200

    fix: finally fix the bulls and cows counting

--- src/dwolf/project_bulls_and_cows/stage_7Of7/Grader.java
+++ src/dwolf/project_bulls_and_cows/stage_7Of7/Grader.java
@@ -13,12 +13,26 @@ class Grader {
     // Whereas a cow is, if the guessed character is part of the secret code, but only at the wrong place.
     protected void gradeGuess(String secretCode, String guess) {
         resetScoresToAvoidCarryover();
+        StringBuilder wasBull = new StringBuilder();
+        StringBuilder wasCow = new StringBuilder();
+
+        // Bulls are checken
         for (int i = 0; i < secretCode.length(); i++) {
             for (int j = 0; j < guess.length(); j++) {
                 if (secretCode.charAt(i) == guess.charAt(j) && i == j) {
+                    wasBull.append(guess.charAt(j));
                     bulls++;
                 }
-                if (secretCode.charAt(i) == guess.charAt(j)) {
+            }
+        }
+
+        // Cows are checked, and count only if the character haven't been either a bull or cow before
+        for (int i = 0; i < secretCode.length(); i++) {
+            for (int j = 0; j < guess.length(); j++) {
+                if (!String.valueOf(wasBull).contains(String.valueOf(guess.charAt(j))) &&
+                        !String.valueOf(wasCow).contains(String.valueOf(guess.charAt(j))) &&
+                        secretCode.charAt(i) == guess.charAt(j)) {
+                    wasCow.append(guess.charAt(j));
                     cows++;
                 }
             }
