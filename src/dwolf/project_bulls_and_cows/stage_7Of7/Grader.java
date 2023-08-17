a04eb499fb2211de5255709b0a83bad682156b73
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-08-18 00:36:03 +0200

    docs: update test to fit current version

--- src/dwolf/project_bulls_and_cows/stage_7Of7/Grader.java
+++ src/dwolf/project_bulls_and_cows/stage_7Of7/Grader.java
@@ -9,8 +9,8 @@ class Grader {
     private int bulls;
     private int cows;
 
-    // A bull is, if the guessed number is at the same position of the generated number.
-    // Whereas a cow is, if the guessed number is part of the generated number, but only at the wrong place.
+    // A bull is, if the guessed character is equal and at the same position as in the secret code.
+    // Whereas a cow is, if the guessed character is part of the secret code, but only at the wrong place.
     protected void gradeGuess(String secretCode, String guess) {
         // Reset bulls and cows ever guess. This flushes results of previous turn.
         bulls = 0;
