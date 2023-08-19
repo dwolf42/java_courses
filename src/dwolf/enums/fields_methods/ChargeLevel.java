32f635eb6c393a77b72abc8a04a2b623f6877f19
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-08-19 17:17:16 +0200

    feat: showcase fields and methods usage in enums

---
+++ src/dwolf/enums/fields_methods/ChargeLevel.java
@@ -0,0 +1,42 @@
+package dwolf.enums.fields_methods;
+
+public enum ChargeLevel {
+    FULL(4, "green"),
+    HIGH(3, "green"),
+    MEDUIUM(2, "yellow"),
+    LOW(1, "red");
+
+    private final int sections;
+    private final String color;
+
+    ChargeLevel(int sections, String color) {
+        this.sections = sections;
+        this.color = color;
+    }
+
+    public static ChargeLevel findChargeLevelByNumberOfSections(int sections) {
+        for (ChargeLevel value : values()) {
+            if (value.sections == sections) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public static String findChargeLevelByColor(String color) {
+        for (ChargeLevel value : values()) {
+            if (value.color.equals(color)) {
+                return value.name();
+            }
+        }
+        return null;
+    }
+
+    public int getSections() {
+        return sections;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/dwolf/enums/fields_methods/FieldsMethods.java
@@ -0,0 +1,11 @@
+package dwolf.enums.fields_methods;
+
+public class FieldsMethods {
+    public static void main(String[] args) {
+        System.out.println(ChargeLevel.LOW.getSections());
+        System.out.println(ChargeLevel.HIGH.getSections());
+        System.out.println(ChargeLevel.findChargeLevelByNumberOfSections(4));
+        System.out.println(ChargeLevel.findChargeLevelByColor("red"));
+    }
+
+}
