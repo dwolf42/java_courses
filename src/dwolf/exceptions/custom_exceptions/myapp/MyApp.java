b56486e6819e5d891a518ef8714078f44dbec81d
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-08-21 19:31:33 +0200

    fix: import java.io.File

--- src/dwolf/exceptions/custom_exceptions/myapp/MyApp.java
+++ src/dwolf/exceptions/custom_exceptions/myapp/MyApp.java
@@ -1,7 +1,11 @@
 package dwolf.exceptions.custom_exceptions.myapp;
 
+import java.io.File;
+
 public class MyApp {
     public static void main(String[] args) throws MyAppException{
+        System.out.println(File.separator);
         throw new MyAppException("Something bad");
+
     }
 }
