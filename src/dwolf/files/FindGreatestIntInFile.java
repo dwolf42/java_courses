6dc11b9ba9c30e9545c4fac0010c269675629db6
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-08-21 19:55:14 +0200

    feat: showcase scanning txt file, evaluate biggest number

---
+++ src/dwolf/files/FindGreatestIntInFile.java
@@ -0,0 +1,31 @@
+package dwolf.files;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FindGreatestIntInFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(findAndReturnGreatestInt());
+    }
+    public static int findAndReturnGreatestInt() throws FileNotFoundException {
+        int greatestNumber = 0;
+        String pathToFile = "\\development\\java\\filehandling\\dataset_91007.txt";
+        File file = new File(pathToFile);
+        Scanner scanner = new Scanner(file);
+
+        try {
+            while(scanner.hasNextInt()) {
+                int temp = scanner.nextInt();
+                if (temp > greatestNumber) {
+                    greatestNumber = temp;
+                }
+
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Cannot read file: " + e.getMessage());
+        }
+
+        return greatestNumber;
+    }
+}
