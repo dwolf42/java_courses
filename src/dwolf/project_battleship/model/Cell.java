1c605c4c980de4fa090e0982058c17b6b5940cda
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2024-01-07 22:45:13 +0100

    set Cell public

--- src/dwolf/project_battleship/model/Cell.java
+++ src/dwolf/project_battleship/model/Cell.java
@@ -1,37 +1,19 @@
 package dwolf.project_battleship.model;
 
 public class Cell {
-    private int row;
-    private int col;
+
     private CellState cellState;
 
-     Cell(int row, int col, CellState cellState) {
-        this.row = row;
-        this.col = col;
+    public Cell(CellState cellState) {
+
         this.cellState = cellState;
     }
 
-     int getRow() {
-        return row;
-    }
-
-     void setRow(int row) {
-        this.row = row;
-    }
-
-     int getCol() {
-        return col;
-    }
-
-     void setCol(int col) {
-        this.col = col;
-    }
-
-     CellState getCellState() {
+    public CellState getCellState() {
         return cellState;
     }
 
-     void setCellState(CellState cellState) {
+    public void setCellState(CellState cellState) {
         this.cellState = cellState;
     }
 }
