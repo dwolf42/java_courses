8da3c6ebd9e9a02de7d1e40643d21efc643edee4
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2024-01-07 14:56:53 +0100

    add notes, remove object creation

--- src/dwolf/project_battleship/GameSetup.java
+++ src/dwolf/project_battleship/GameSetup.java
@@ -1,11 +1,18 @@
-package dwolf.project_battleship;
+/*
+* Gameboard contains Ship objects, which have data about
+* their coordinates and how many hits taken/sunken.
+*
+* How are ships going to get placed at the beginning of the game?
 
-import dwolf.project_battleship.controller.GameInputController;
-import dwolf.project_battleship.model.OceanModel;
+How will you show a user their gameplay grid?
+* How will you know when a ship is sunk?
+* How will you know when all ships are sunk, and the game is over?
+*
+* */
+package dwolf.project_battleship;
 
 public class GameSetup {
     public static void main(String[] args) {
-        OceanModel oceanModel = new OceanModel();
-        GameInputController gameInputController = new GameInputController()
+
     }
 }
