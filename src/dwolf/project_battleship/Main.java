4efcdbccadf96c5fbb5985745344961bf012fc68
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2024-03-17 20:13:26 +0100

    move initialization to controller class

--- src/dwolf/project_battleship/Main.java
+++ src/dwolf/project_battleship/Main.java
@@ -1,21 +1,21 @@
 package dwolf.project_battleship;
 
-import dwolf.project_battleship.model.Ocean;/*
- * Basically, and ocean consists of quadrants, like 1x1 meters.
- * Each quadrant can have a state, like being water, a ship, a hit ship, or a miss.
- * In order to know where each quadrant is located on the ocean, it has individual coordinates, like 1,3, 2,5 etc.
- * The state is the lowest level of the hierarchy, but contains the most valuable variable.
- * The quadrant is the next higher level in the hierarchy,
- *
- *
- * */
+/*
+Basically, and ocean consists of quadrants, like 1x1 meters.
+* Each quadrant can have a state, like being water, a ship, a hit ship, or a miss.
+* In order to know where each quadrant is located on the ocean, it has individual coordinates, like 1,3, 2,5 etc.
+* The state is the lowest level of the hierarchy, but contains the most valuable variable.
+* The quadrant is the next higher level in the hierarchy,
+*
+*
+* */
+
+import dwolf.project_battleship.controller.Navigation;
 
 public class Main {
     public static void main(String[] args) {
-        int row = 10;
-        int col = 10;
-        Ocean ocean = new Ocean(row, col);
-
+        Navigation navigation = new Navigation();
+        navigation.initialize();
     }
 
 }
--- src/dwolf/project_battleship/controller/Navigation.java
+++ src/dwolf/project_battleship/controller/Navigation.java
@@ -1,4 +1,14 @@
 package dwolf.project_battleship.controller;
 
+import dwolf.project_battleship.model.Ocean;
+import dwolf.project_battleship.view.Radar;
+
 public class Navigation {
+    public void initialize() {
+        int row = 10;
+        int col = 10;
+        Ocean ocean = new Ocean(row, col);
+        Radar radar = new Radar();
+        radar.detect(ocean.getQuadrant());
+    }
 }
