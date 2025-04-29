6d9566398fc055c062989ed5d1093cfc7af1a3b6
David Wolf <46925680+dwolf42@users.noreply.github.com>
2025-04-29 19:18:47 +0200

    add: example from today's tutoring about static class attributes

---
+++ src/dwolf/school/static_attributes_methods/circle_static/SchoolCircle.java
@@ -0,0 +1,14 @@
+package dwolf.school.static_attributes_methods.circle_static;
+
+public class SchoolCircle {
+    private static double PI = 3.14159;
+    private double radius;
+
+    SchoolCircle(double radius) {
+        this.radius = radius;
+    }
+
+    double getArea() {
+        return PI * radius * radius;
+    }
+}
