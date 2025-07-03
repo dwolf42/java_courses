411f6399b85b5567eed7c69edea759e0a699f886
David Wolf <dwolf@starface.com>
2025-07-03 19:14:52 +0200

    initial commit

---
+++ src/dwolf/challenges/reddit/user_info_to_file/InfoToFile.java
@@ -0,0 +1,7 @@
+﻿package dwolf.challenges.reddit.user_info_to_file;
+
+public class InfoToFile {
+   public void run() {
+
+   }
+}
---
+++ src/dwolf/challenges/reddit/user_info_to_file/Runner.java
@@ -0,0 +1,8 @@
+﻿package dwolf.challenges.reddit.user_info_to_file;
+
+public class Runner {
+   public static void main(String args[]) {
+       InfoToFile itf = new InfoToFile();
+       itf.run();
+   }
+}
