64f297d128b05b49803e892ba5f572b72b751b80
David Wolf <dwolf@starface.com>
2025-07-09 16:48:05 +0200

    add: file reading exercises

--- src/dwolf/challenges/reddit/user_info_to_file/InfoToFile.java
+++ src/dwolf/challenges/reddit/user_info_to_file/InfoToFile.java
@@ -1,5 +1,11 @@
 package dwolf.challenges.reddit.user_info_to_file;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import java.util.Scanner;
 
 public class InfoToFile {
@@ -7,10 +13,42 @@ public class InfoToFile {
     private int userAge;
     private String userRedditName;
 
-    public void run() {
+    public void run() throws IOException {
+        readFileNIO();
+        readFileIO();
+        askQuestions();
+    }
+
+    public void readFileNIO() {
+        String pathToFile = "C:\\Users\\dwolf\\Documents\\coding\\dummies\\for-scanner-to-read.txt";
+
+        try {
+            byte[] allBytesRead = Files.readAllBytes(Paths.get(pathToFile));
+            String readFileAsString = new String(allBytesRead);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void readFileIO() throws FileNotFoundException {
+        String pathToFile = "\\\\\\C:\\Users\\dwolf\\Documents\\coding\\dummies\\for-scanner-to-read.txt";
+        File file = new File(pathToFile);
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine() + " ");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("No file found: " + pathToFile);
+        }
+        System.out.println();
+    }
+
+    public void askQuestions() {
         System.out.println("Hi, my name is Brandigasco.");
         System.out.println("What is your name...?");
-        System.out.println("");
+        System.out.println();
         Scanner scanner = new Scanner(System.in);
         userName = scanner.nextLine();
         System.out.println("So, " + userName + " what a lovely name!");
@@ -29,5 +67,4 @@ public class InfoToFile {
         System.out.println("So, your name is " + userName + ", you are " + userAge + " years old, "
                 + "and your username is " + userRedditName);
     }
-
 }
