fad9fb8c75f5959a18f00db64d403e870acd2998
David Wolf <dwolf@starface.com>
2025-07-11 19:36:09 +0200

    chore: correct notation

--- src/dwolf/challenges/reddit/user_info_to_file/Runner.java
+++ src/dwolf/challenges/reddit/user_info_to_file/Runner.java
@@ -3,7 +3,7 @@ package dwolf.challenges.reddit.user_info_to_file;
 import java.io.IOException;
 
 public class Runner {
-   public static void main(String args[]) throws IOException {
+   public static void main(String[] args) throws IOException {
        InfoToFile itf = new InfoToFile();
        itf.run();
    }
