46529e06faf2fc7a2cfc29d9d4342395dcc134b4
David Wolf <46925680+dwolf42@users.noreply.github.com>
2025-07-15 22:10:26 +0200

    refactor: move code to BattleshipGame class

--- src/dwolf/project_battleship/BattleshipGame.java
+++ src/dwolf/project_battleship/BattleshipGame.java
@@ -2,46 +2,59 @@ package dwolf.project_battleship;
 
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Map;
-import java.util.LinkedHashMap;
 
-public class Main {
-    public static void main(String[] args) {
-        // 65 = A => 0
-        // 66 = B => 1
-        // 67 = C => 2
-        // 68 = D => 3
-        // 69 = E => 4
-        // 70 = F => 5
-        // 71 = G => 6
-        // 72 = H => 7
-        // 73 = I => 8
-        // 74 = J => 9
+public class BattleshipGame {
+    // 65 = A => 0
+    // 66 = B => 1
+    // 67 = C => 2
+    // 68 = D => 3
+    // 69 = E => 4
+    // 70 = F => 5
+    // 71 = G => 6
+    // 72 = H => 7
+    // 73 = I => 8
+    // 74 = J => 9
 
-        char[][] gameMap = new char[10][10];
+    private char[][] gameMap;
+
+    public BattleshipGame() {
+        gameMap = new char[10][10];
 
         for (char[] location : gameMap) {
             Arrays.fill(location, '~');
         }
+    }
 
+    public void run() {
         printMap(gameMap);
+        obtainCoords();
+    }
+
+    private int[] obtainCoords() {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         String castUpper = userInput.toUpperCase();
 
+        int[] coords = new int[4];
+
         char letterPart = castUpper.charAt(0);
         int letterToInt = letterPart;
-        int row = letterToInt - 65;
+        coords[0] = letterToInt - 65;
 
         char numPart = userInput.charAt(1);
-        int col = Character.getNumericValue(numPart) - 1;
-        scanner.close();
-        System.out.println(col);
+        coords[1] = Character.getNumericValue(numPart) - 1;
 
+        letterPart = castUpper.charAt(3);
+        letterToInt = letterPart;
+        coords[2] = letterToInt - 65;
+
+        numPart = userInput.charAt(4);
+        coords[3] = Character.getNumericValue(numPart) - 1;
+
+        return coords;
     }
 
-    private static void printMap(char[][] gameMap) {
-
+    private void printMap(char[][] gameMap) {
         // Add numbers 1-10 above game board for column enumeration
         for (int i = 0; i < gameMap.length; i++) {
             System.out.print((i == 0 ? "  " : " ") + (i + 1));
@@ -60,4 +73,5 @@ public class Main {
             System.out.println();
         }
     }
+
 }
