fb37c97aa67b36b1cb2ed151c202d2a0df0c112c
David Wolf <dwolf@starface.com>
2025-09-01 17:26:12 +0200

    chore: formatting

--- src/dwolf/booleans/Boolean_BoysInASportClass.java
+++ src/dwolf/booleans/Boolean_BoysInASportClass.java
@@ -19,7 +19,9 @@ public class Boolean_BoysInASportClass {
 /*
 Suppose there are three boys in a sports class.
 
-You need to write a program that checks the boys are arranged in the ascending or descending order by height. The program must read three integer numbers h1, h2, h3 and outputs true or false. If boys have the same height, they are considered as correctly arranged.
+You need to write a program that checks the boys are arranged in the ascending or descending order by height.
+The program must read three integer numbers h1, h2, h3 and outputs true or false.
+If boys have the same height, they are considered as correctly arranged.
 
 Sample Input 1:
 165 161 158
