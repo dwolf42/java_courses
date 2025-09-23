81446db68f217e0a3612043bad5886b50560b7e0
David Wolf <46925680+dwolf42@users.noreply.github.com>
2025-09-23 10:57:09 +0200

    initial commit

---
+++ src/dwolf/school/fintness_center/FintessCenter.java
@@ -0,0 +1,38 @@
+package dwolf.school.fintness_center;
+
+public class FintessCenter {
+    private String name;
+    private Kunde[] kunden;
+
+
+    public FintessCenter(String name) {
+        super();
+        this.name = name;
+        kunden = new Kunde[10];
+    }
+
+    public Kunde neuerKunde(String id, String name, double gewicht, double groesse) {
+        for (int i = 0; i < kunden.length; i++) {
+            if (kunden[i] == null) {
+                kunden[i] = new Kunde(id, name, gewicht, groesse);
+                return kunden[i]; // gibt den einen Kunden an diesem Index zurück
+            }
+        }
+
+        return null;
+    }
+
+    public Kunde neuerKunde(String id, String name) {
+        return neuerKunde(id, name, 0.0, 0.0); // man kann auf den anderen Konstruktor zugreifen
+    }
+
+    public Kunde loescheKunde(String id) {
+        return Kunde;
+    }
+
+    public void print() {
+        for (Kunde kund : kunden) {
+            System.out.println(kund);
+        }
+    }
+}
