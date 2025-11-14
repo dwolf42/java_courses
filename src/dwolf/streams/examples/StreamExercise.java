5de000c7c5f8c705ea7143a0051ae4d9c4a91881
Dwolf <46925680+dwolf42@users.noreply.github.com>
2025-11-14 19:49:47 +0100

    add: exercise traditional counting vs stream counting

---
+++ src/dwolf/streams/examples/StreamExercise.java
@@ -0,0 +1,32 @@
+package dwolf.streams.examples;
+
+import java.util.HashMap;
+import java.util.List;
+
+class StreamExercise {
+	public static void main(String[] args) {
+		// Task: Count numbers greater than 5
+		List<Integer> numbers = List.of(1, 4, 7, 6, 2, 9, 7, 8);
+
+		// Traditional approach
+		long tradCount = 0;
+		for (int number : numbers) {
+			if (number > 5) {
+				tradCount++;
+			}
+		}
+		System.out.println("Traditional: " + tradCount);
+
+		// Stream approach
+		long streamCount = numbers.stream()
+			// add a skip(4) to skip the first 4 numbers
+			// reads the same as a for each loop:
+			// for each number do number > 5 count
+			.filter(number -> number > 5) // predicate lambda expression
+			.count(); // 5
+
+		System.out.println("Stream: " + streamCount);
+
+	}
+
+}
