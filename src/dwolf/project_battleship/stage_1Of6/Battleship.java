4a893398bd4f926f8778e2f10a965aa1d8b514de
David Wolf <46925680+dwolf42@users.noreply.github.com>
2026-07-19 18:43:31 +0200

    wip: try to fix parsing

--- src/dwolf/project_battleship/stage_1Of6/Battleship.java
+++ src/dwolf/project_battleship/stage_1Of6/Battleship.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Battleship {
     public Battleship() {
+        System.out.println(Integer.parseInt("G"));
         System.out.print(" ");
         for (int i = 1; i <= 10; i++) {
             System.out.print(" ");
@@ -26,17 +28,23 @@ public class Battleship {
     usrInput = scanner.nextLine().toUpperCase();
     String[] inputParts = usrInput.split(" ");
 
+    // The ASCII character code (int representation) for 'A' is 65
+    // The first index of an array is 0.
+    // Subtracting 65 from the int rep. will result in the correct index.
     int x1 = (int) inputParts[0].charAt(0) - 65;
     String y1Part = inputParts[0].substring(1);
     int y1 = Integer.parseInt(y1Part) - 1;
     
-    int x2 = (int) inputParts[1].charAt(1);
+    int x2 = (int) inputParts[1].charAt(1) - 65;
     String y2Part = inputParts[1].substring(1);
     int y2 = Integer.parseInt(y2Part) - 1;
-    System.out.print(x1);
-    System.out.println(y1);
-    System.out.print(x2);
-    System.out.println(y2);
+
+    System.out.println("input parts1: " + inputParts[0]);
+    System.out.println("input parts2: " + inputParts[1]);
+    System.out.println("x1: " + x1);
+    System.out.println("y1: " + y1);
+    System.out.println("x2: " + x2);
+    System.out.println("y2: " + y2);
     // 65 74
     }
 
